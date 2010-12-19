26b6a110705bad597e8ab7fda2d121ee8ca71c22
Mike Sutton <mike.sutton@wizewerx.com>
2010-12-19 01:05:50 +0100

    Done with the demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayWithRails::Application.config.secret_token = '9beb2b64826f55c887f219d18cb33ce88e7fb1c0c32c4ee92fbe11f8618aaa4605c27f00526aa6f9dc47f36eacacbc62fb60eee2c3570ec91fd7b1eea3ae9014'
